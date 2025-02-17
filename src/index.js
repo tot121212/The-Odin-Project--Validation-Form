c08f026a60d92776ac0fe6a1fcb1ef97218c5e54
tot121212 <totmusicbusiness@gmail.com>
2025-02-16 19:48:54 -0600

    rename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     form.addEventListener("submit", (event) => {
         event.preventDefault();
         const formData = new FormData(form);
-        const data = Object.fromEntries(formData);
-        console.log(data);
+        const formDataObj = Object.fromEntries(formData);
+        console.log(formDataObj);
     });
 });
