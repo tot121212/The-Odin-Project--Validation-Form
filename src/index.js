b6774c7e6fc76a7d77bd52c93d25f8f63cd10f9d
tot121212 <totmusicbusiness@gmail.com>
2025-02-16 19:46:15 -0600

    js imports, domcontentloaded event listener for submit form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!")
+import "./css-reset.css";
+import "./template.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData);
+        console.log(data);
+    });
+});
